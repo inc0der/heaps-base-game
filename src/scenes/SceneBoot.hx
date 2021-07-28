92a7e6b4f7025370c30df592bc942c16804924c1
inc0der <inc0der.code@gmail.com>
2021-07-28 11:04:39 -0300

    Add default boot scene

---
+++ src/scenes/SceneBoot.hx
@@ -0,0 +1,37 @@
+package scenes;
+
+import entities.Sprite;
+import h2d.col.Bounds;
+import h2d.Bitmap;
+
+class SceneBoot extends Scene {
+  public var logo: Bitmap;
+
+  public override function init() {
+    addLogo();
+  }
+
+  public function addLogo() {
+    var tile = Res.img.ltn_logo.toTile();
+    logo = new Bitmap(tile);
+    logo.scaleX = 0.4;
+    logo.scaleY = 0.4;
+
+    var size = logo.getSize();
+    logo.y = height / 2 - size.height / 2;
+    logo.x = width / 2 - size.width / 2;
+
+    addChild(logo);
+  }
+
+  public override function update(dt: Float) {
+    super.update(dt);
+    if (logo != null) {
+      if (logo.alpha > 0) {
+        logo.alpha -= 0.15 * dt;
+      }
+    }
+  }
+
+  public override function dispose() {}
+}
