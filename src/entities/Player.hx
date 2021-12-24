530431b20f1d97611ee889f3315c9a97384649f3
inc0der <inc0der.dev@gmail.com>
2021-12-24 15:19:22 -0400

    Add basic input to the player class

--- src/entities/Player.hx
+++ src/entities/Player.hx
@@ -5,17 +5,30 @@ import h2d.Tile;
 
 class Player extends Sprite {
   public var graphic: Tile;
+  public var speed: Int;
 
   public function new(x, y, ?parent) {
     graphic = Res.img.player.toTile();
     super(x, y, graphic, true, parent);
+
+    speed = 5;
   }
 
   public override function onKeyDown(event: Event) {
-    trace('DOWN keyCode: ${event.keyCode}, charCode: ${event.charCode}');
-  }
+    if (event.keyCode == 87 || event.keyCode == 38) {
+      this.y -= speed;
+    }
 
-  public override function onKeyUp(event: Event) {
-    trace('UP keyCode: ${event.keyCode}, charCode: ${event.charCode}');
+    if (event.keyCode == 83 || event.keyCode == 40) {
+      this.y += speed;
+    }
+
+    if (event.keyCode == 65 || event.keyCode == 37) {
+      this.x -= speed;
+    }
+
+    if (event.keyCode == 68 || event.keyCode == 39) {
+      this.x += speed;
+    }
   }
 }
