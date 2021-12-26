5d5afd51c6a10ec4ae6a6b33e2d42d1a42011def
inc0der <inc0der.dev@gmail.com>
2021-12-26 11:54:15 -0400

    Add select and ok sounds to title scene

--- src/scenes/Title.hx
+++ src/scenes/Title.hx
@@ -1,5 +1,6 @@
 package scenes;
 
+import hxd.res.Sound;
 import hxd.System;
 import core.Scene;
 
@@ -8,13 +9,17 @@ class Title extends Scene {
   public var playText: h2d.Text;
   public var quitText: h2d.Text;
   public var optionsText: h2d.Text;
+  public var uiOk: Sound;
+  public var uiSelect: Sound;
 
   public override function init() {
+    uiSelect = Res.audio.se.ui_select;
+    uiOk = Res.audio.se.ui_ok;
     addTitleText();
     addCommandButtons();
   }
 
-  public function createTextButton(title: String, size: Int, onPress) {
+  public function createTextButton(title: String, size: Int, onPress, ?onOver) {
     var font = hxd.res.DefaultFont.get().clone();
     font.resizeTo(size);
 
@@ -23,6 +28,7 @@ class Title extends Scene {
 
     var interaction = new h2d.Interactive(textButton.textWidth, textButton.textHeight, textButton);
     interaction.onClick = onPress;
+    interaction.onOver = onOver;
     return textButton;
   }
 
@@ -36,18 +42,28 @@ class Title extends Scene {
   }
 
   public function addCommandButtons() {
-    playText = createTextButton("Start", 24, onPlay);
-    quitText = createTextButton("Quit", 24, onExit);
+    playText = createTextButton("Start", 24, onPlay, onOverPlay);
+    quitText = createTextButton("Quit", 24, onExit, onOverExit);
     playText.setPosition(width / 2 - playText.textWidth / 2, height / 2);
     quitText.setPosition(width / 2 - quitText.textWidth / 2, height / 2 + playText.textHeight + 10);
   }
 
   public function onPlay(event) {
+    uiOk.play();
     game.changeScene(new scenes.World());
   }
 
   public function onExit(event) {
+    uiOk.play();
     System.exit();
+  }
+
+  public function onOverPlay(event) {
+    uiSelect.play();
+  }
+
+  public function onOverExit(event) {
+    uiSelect.play();
   }
 
   public override function onResize() {
